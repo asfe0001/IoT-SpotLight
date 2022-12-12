48d5b317f4c1ba76e95f312fb0576068fb64c9d8
lijo1022 <118208964+lijo1022@users.noreply.github.com>
2022-12-12 18:54:41 +0100

    add work in progress Lamp1 code

---
+++ Lamp_1/Spotlight_Lamp1.py
@@ -0,0 +1,88 @@
+# system library
+import RPi.GPIO as GPIO
+import time
+
+# for LED pwm brightness 
+from time import sleep # Import the sleep function from the time module
+
+# mqtt library
+import paho.mqtt.client as mqtt
+
+# MQTT Daten
+username=""
+password=""
+address="test.mosquitto.org"
+
+# connect
+client = mqtt.Client()
+client.username_pw_set(username, password)
+client.connect(address, 1883, 60)
+
+
+
+#INIT ultrasonic Sensor
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+# multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+
+#INIT LED PWM
+
+LEDPIN = 22
+GPIO.setup(LEDPIN, GPIO.OUT) # Set pin 8 to be an output pin and set initial value to low (off)
+LED = GPIO.PWM(LEDPIN, 100)
+
+LED.start(0)
+
+#variables
+pos1 = False
+pos2 = False
+
+# TO DO: subscribe to pos2 here
+
+#do ForEVER
+while True:
+    pos1 = distance() < 200
+    if pos1:
+        LED.ChangeDutyCycle(100) # LED an
+    else:
+        if pos2:
+            LED.ChangeDutyCycle(50) # LED halb hell
+        else:
+            LED:ChangeDutyCycle(0) # LED aus
+
+    # TO DO: publish pos1
+    # evtl TO DO: fade between LED Levels
